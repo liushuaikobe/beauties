22b9c95394fc7347cbd7f76f657f6d8be8392466
liushuaikobe <liushuaikobe@gmail.com>
2015-08-05 21:55:16 +0800

    add fade in animation to the history image

--- beauties/BeautyCollectionViewCell.swift
+++ beauties/BeautyCollectionViewCell.swift
@@ -26,6 +26,11 @@ class BeautyCollectionViewCell: UICollectionViewCell {
         commonInit()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.alpha = 0
+    }
+    
     func commonInit() -> Void {
         self.clipsToBounds = false
         self.layer.borderWidth = 10
@@ -35,15 +40,22 @@ class BeautyCollectionViewCell: UICollectionViewCell {
         self.layer.shadowOffset = CGSizeMake(2, 6)
         
         self.imageView.clipsToBounds = true
+        self.imageView.frame = self.bounds
+        self.imageView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        self.imageView.contentMode = .ScaleAspectFill
         self.addSubview(self.imageView)
     }
     
     func bindData(entity: BeautyImageEntity) -> Void {
-        self.imageView.frame = self.bounds
-        self.imageView.contentMode = .ScaleAspectFill
+        
         if let urlString = entity.imageUrl {
             if let url = NSURL(string: urlString) {
-                self.imageView.kf_setImageWithURL(url)
+                self.imageView.kf_setImageWithURL(url, placeholderImage: nil, optionsInfo: nil, completionHandler: {
+                    [weak self](image, error, cacheType, imageURL) -> () in
+                    UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseIn, animations: { () -> Void in
+                        self?.imageView.alpha = 1
+                        }, completion: nil)
+                })
             }
         }
     }
